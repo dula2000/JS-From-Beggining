a98af16e98322c8d89a639b678c176f00f7500ea
dula2000 <96098056+dula2000@users.noreply.github.com>
2022-11-26 08:09:51 +0530

    Variables types

---
+++ index.js
@@ -0,0 +1,23 @@
+//window.alert("I love pizza");
+
+/*  --------01. variable----------
+
+
+1. declarations (var,let,const)
+
+
+
+let name="dds";
+let age = 22;
+let studentage= 25;
+
+console.log("Hello machan",name);
+console.log("I am" ,age, "years old");
+console.log(studentage);
+
+document.getElementById("p1").innerHTML = "Hello " + name;
+document.getElementById("p2").innerHTML = "Hello, I'm " +age+ " years old";
+document.getElementById("p3").innerHTML = "student age is " +studentage;
+*/
+
+/* ----------02. Arithmatic Expressions----------- */
