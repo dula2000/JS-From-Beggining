f3b3dd240ed917bfffe69fe2b461370b8b7f3211
dula2000 <96098056+dula2000@users.noreply.github.com>
2022-12-03 09:32:55 +0530

    shuffle an array

--- index.js
+++ index.js
@@ -391,3 +391,26 @@ console.log(`${precent(45,50)}`);
 
 
 /*----------shuffle an array---------- */
+
+let cards =["A","1","2","3","4","5","6","7","8","9","10"];
+shuffle(cards);
+//console.log(cards);
+cards.forEach(card=>console.log(card));
+
+
+function shuffle(array){
+  let currentIndex = array.length;
+
+  while(currentIndex !=0){
+    let randomIndex = Math.floor(Math.random()* array.length);
+    currentIndex -=1;
+
+    let temp = array[currentIndex];
+    array[currentIndex]= array[randomIndex];
+    array[randomIndex]= temp;
+  }
+  return array;
+}
+ 
+
+/*----------Nested Function---------- */
