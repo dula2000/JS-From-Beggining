8479fad7dcb5e63a934ce6b742fd39709a1a37e9
dula2000 <96098056+dula2000@users.noreply.github.com>
2022-12-04 18:10:28 +0530

    class

--- index.js
+++ index.js
@@ -433,7 +433,7 @@ store.forEach((value,key)=>console.log(`${key} $${value}`));
 
 */
 
-/*----------objects--------- */
+/*----------objects--------- 
 
 
 const car ={
@@ -464,5 +464,32 @@ const car22 ={
    }
  }
 
-
 car22.paddle();
+*/
+
+/*----------class--------- */
+
+// class = a blueprint for creating objects define what properties and methods they have
+//            use a constructor for unique properties.
+
+
+class Player{
+  score = 0;
+  pause(){
+    console.log("you passed the game");
+  }
+  exit(){
+    console.log("You exited the game");
+  }
+}
+
+const player1 = new Player();
+const player2 = new Player();
+
+player1.score +=1;
+
+console.log(player1.score);
+console.log(player2.score);
+
+player1.pause();
+player2.exit();
