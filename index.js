38fc7ef1435ca87c5565bdb1472e8069f4e4235d
dula2000 <96098056+dula2000@users.noreply.github.com>
2022-12-10 08:59:53 +0530

    Anonymous Objects

--- index.js
+++ index.js
@@ -640,7 +640,7 @@ console.log(car.gas);
 */
 
 
-/*----------objects as arguments--------- */
+/*----------objects as arguments--------- 
 
 class Car{
 
@@ -666,4 +666,26 @@ function displayInfo(car){
 function changeColor(car, color){
   car.color = color;
 }
+
+*/
+
+/*----------Anonymous Objects--------- */
+
+class Card{
+    constructor(value, suit){
+        this.value = value;
+        this.suit = suit;
+    }
+}
+
+let cards = [new Card("A", "Hearts"), 
+             new Card("A", "Spades"), 
+             new Card("A", "Diamonds"), 
+             new Card("A", "Clubs"), 
+             new Card("2", "Hearts"), 
+             new Card("2", "Spades"), 
+             new Card("2", "Diamonds"), 
+             new Card("2", "Clubs")];
+
+cards.forEach(card => console.log(`${card.value} ${card.suit}`));
 
