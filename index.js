6c8b94409ebdc0eb3635e2fa14936222ebc3ff31
dula2000 <96098056+dula2000@users.noreply.github.com>
2022-12-11 10:12:32 +0530

    console.time()

--- index.js
+++ index.js
@@ -830,3 +830,18 @@ function formatTime(date){
 console.log("start");
 setTimeout(()=>console.log("This is asynchronus"),5000);
 console.log("END");
+
+/*----------console.time() --------- */
+
+// console.time() = Starts a timer you can use to 
+//                  track how long an operation takes
+//                  Give each timer a unique name.
+
+//start
+console.time("Response time");
+
+alert("CLICK THE OK BUTTON!");
+
+//end
+console.timeEnd("Response time");
+Transcript
