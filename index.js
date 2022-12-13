c9c6e66094c25fcdbd27d0023df5d465be516ab2
dula2000 <96098056+dula2000@users.noreply.github.com>
2022-12-13 08:30:30 +0530

    ES6 Module-

--- index.js
+++ index.js
@@ -911,8 +911,19 @@ loadFile();
 */
 
 
-/*----------async and await--------- */
+/*----------ES6 Module--------- */
+//import {PI, getCircumference, getArea} from "./math_util.js";
+import * as MathUtil from "./math_util.js";
 
+console.log(MathUtil.PI);
+
+let circumference = MathUtil.getCircumference(10);
+console.log(circumference);
+
+let area = MathUtil.getArea(10);
+console.log(area);
+
+/*----------ES6 Module--------- */
 
 
 
