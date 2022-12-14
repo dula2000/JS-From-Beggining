dae92a3848df1e8a181bbc8c7544dc37962a0241
dula2000 <96098056+dula2000@users.noreply.github.com>
2022-12-14 08:11:51 +0530

    addEventListener()

--- index.js
+++ index.js
@@ -1000,6 +1000,21 @@ function doSomethingElse(){
 }
 */
 
-/*----------events-------- */
+/*----------addEventListener()-------- */
+
+// .addEventListener(event, function, useCapture)
+// You can add many event handlers to one element.
+// Even the same event that invokes different functions
+
+const innerDiv = document.getElementById("innerDiv");
+const outerDiv = document.getElementById("outerDiv");
+
+innerDiv.addEventListener("click", changeBlue);
+outerDiv.addEventListener("click", changeBlue, true);
+
+function changeBlue(){
+    alert(`You selected ${this.id}`);
+    this.style.backgroundColor = "lightblue"
+}
 
 
