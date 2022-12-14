49c1f10746242f345b5aaffa57505638f98849f9
dula2000 <96098056+dula2000@users.noreply.github.com>
2022-12-14 07:34:09 +0530

    add/change HTML selectors

--- index.js
+++ index.js
@@ -957,5 +957,23 @@ elements.forEach(element => {
 })
 */
 
+/*----------add/change HTML selectors--------- */
+
+// add/change HTML elements
+// .innerHTML (vulnerable to XSS attacks)
+// .textContent (more secure)
+
+const nameTag = document.createElement("h1");
+nameTag.textContent = window.prompt("enter your name");
+document.body.append(nameTag);
+
+//const myList = document.querySelector("#fruit");
+//const listItem = document.createElement("li");
+//listItem.textContent = "mango";
+//myList.append(listItem);
+//myList.prepend(listItem);
+//myList.insertBefore(listItem, myList.getElementsByTagName("li")[1]);
+
+
 
 
