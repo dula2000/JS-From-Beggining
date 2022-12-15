2fd685d1f268364b30f588ac01e06086526492aa
dula2000 <96098056+dula2000@users.noreply.github.com>
2022-12-15 09:18:06 +0530

    detect key presses

--- index.js
+++ index.js
@@ -1032,4 +1032,36 @@ myButton.addEventListener("click", () => {
 })
 */
 
-/*----------show/hide HTML elements-------- */
+/*----------detect key presses-------- 
+
+const myDiv = document.getElementById("myDiv");
+window.addEventListener("keydown", move);
+let x = 0;
+let y = 0;
+
+function move(event){
+    
+    switch(event.key){
+        case "ArrowDown":
+            y+=5;
+            myDiv.style.top = y + "px";
+            break;
+        case "ArrowUp":
+            y-=5;
+            myDiv.style.top = y + "px";
+            break;
+        case "ArrowRight":
+            x+=5;
+            myDiv.style.left = x + "px";
+            break;
+        case "ArrowLeft":
+            x-=5;
+            myDiv.style.left = x + "px";
+            break;
+        default:
+            break;
+    }
+}
+*/
+/*----------Animation-------- */
+
