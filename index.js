833163d6d6342061a19dde3c9c23d09d371a04e2
dula2000 <96098056+dula2000@users.noreply.github.com>
2022-12-17 04:11:55 +0530

    Cookies

--- index.js
+++ index.js
@@ -1202,3 +1202,45 @@ context.fillText("YOU WIN!", canvas.width / 2, canvas.height / 2);
 //window.location.href= "https://google.com";
 
 //console.log(window.location.href);
+
+
+/*----------canvas API-------- */
+
+//Cookie= a small text file stored on your computer used
+//      to remember information about the user saved in name-value pairs
+
+const firstText = document.querySelector("#firstText");
+const lastText = document.querySelector("#lastText");
+const submitBtn = document.querySelector("#submitBtn");
+const cookieBtn = document.querySelector("#cookieBtn");
+
+submitBtn.addEventListener("click", () => {
+    setCookie("firstName", firstText.value, 365);
+    setCookie("lastName", lastText.value, 365);
+});
+cookieBtn.addEventListener("click", () => {
+    firstText.value = getCookie("firstName");
+    lastText.value = getCookie("lastName");
+});
+
+function setCookie(name, value, daysToLive){
+    const date = new Date();
+    date.setTime(date.getTime() +  (daysToLive * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + date.toUTCString();
+    document.cookie = `${name}=${value}; ${expires}; path=/`
+}
+function deleteCookie(name){
+    setCookie(name, null, null);
+}
+function getCookie(name){
+    const cDecoded = decodeURIComponent(document.cookie);
+    const cArray = cDecoded.split("; ");
+    let result = null;
+    
+    cArray.forEach(element => {
+        if(element.indexOf(name) == 0){
+            result = element.substring(name.length + 1)
+        }
+    })
+    return result;
+}
